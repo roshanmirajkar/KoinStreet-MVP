e6456c68cd97da56523f2d7e188e39b5b8c85947
kcpx <kc.pratik7@gmail.com>
2019-09-30 17:25:19 -0400

    Coinbase portfolio UI print

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -16,7 +16,9 @@ class App extends React.Component {
 			expires_in: '',
 			refresh_token: '',
 			created_at: '',
-			error: ''
+			error: '',
+			balance: [],
+			wallet:[],
 		}
 	}
 	render() {
@@ -63,6 +65,7 @@ class App extends React.Component {
 								//			console.log(newStringResponse.data.data[i].balance.amount)
 											var balance = newStringResponse.data.data[i].balance.amount;
 											var walletName = newStringResponse.data.data[i].name;
+											
 									//		console.log(balance)
 											var currency = newStringResponse.data.data[i].currency;
 											this.setState({balance:balance})
@@ -70,9 +73,44 @@ class App extends React.Component {
 											console.log(this.state.balance)
 											console.log(this.state.walletName)
 											i++;
+											document.getElementById("balance").innerHTML =  newStringResponse.data.data[0].name + '\n' +
+											+ '\n' + newStringResponse.data.data[0].balance.amount + '\n'+ 
+											newStringResponse.data.data[1].name +
+											+ " " + newStringResponse.data.data[1].balance.amount;
+											// newStringResponse.data.data[2].name 
+											// + " " + newStringResponse.data.data[2].balance.amount +
+											// newStringResponse.data.data[3].name 
+											// + " " + newStringResponse.data.data[3].balance.amount +
+											// newStringResponse.data.data[4].name 
+											// + " " + newStringResponse.data.data[4].balance.amount +
+											// newStringResponse.data.data[5].name 
+											// + " " + newStringResponse.data.data[5].balance.amount +
+											// newStringResponse.data.data[6].name 
+											// + " " + newStringResponse.data.data[6].balance.amount +
+											// newStringResponse.data.data[7].name 
+											// + " " + newStringResponse.data.data[7].balance.amount +
+											// newStringResponse.data.data[8].name 
+											// + " " + newStringResponse.data.data[8].balance.amount +
+											// newStringResponse.data.data[9].name 
+											// + " " + newStringResponse.data.data[9].balance.amount +
+											// newStringResponse.data.data[10].name 
+											// + " " + newStringResponse.data.data[10].balance.amount +
+											// newStringResponse.data.data[11].name 
+											// + " " + newStringResponse.data.data[11].balance.amount +
+											// newStringResponse.data.data[12].name 
+											// + " " + newStringResponse.data.data[12].balance.amount +
+											// newStringResponse.data.data[13].name 
+											// + " " + newStringResponse.data.data[13].balance.amount +
+											// newStringResponse.data.data[14].name 
+											// + " " + newStringResponse.data.data[14].balance.amount +
+											// newStringResponse.data.data[15].name 
+											// + " " + newStringResponse.data.data[15].balance.amount +
+											// newStringResponse.data.data[16].name 
+											// + " " + newStringResponse.data.data[16].balance.amount +
+											// newStringResponse.data.data[17].name 
+											// + " " + newStringResponse.data.data[17].balance.amount;
+
 										}
-									//document.getElementById("balance").innerHTML = "Hello "+ name +" your " + walletName + 
-								//	" balance is "+ balance + " " + currency + " or $ ";
 								}
 							})
 
